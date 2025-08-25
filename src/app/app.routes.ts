58352fc2c58fae1178bc91edc423a506453b6076
ck9developer <ck9develop@gmail.com>
2025-08-25 14:39:13 +0200

    add tabs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,24 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'walks',
+    loadComponent: () => import('./pages/walks/walks.page').then( m => m.WalksPage)
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
+  },
+  {
+    path: 'social',
+    loadComponent: () => import('./pages/social/social.page').then( m => m.SocialPage)
+  },
+  {
+    path: 'settings',
+    loadComponent: () => import('./pages/settings/settings.page').then( m => m.SettingsPage)
+  },
+  {
+    path: 'health',
+    loadComponent: () => import('./pages/health/health.page').then( m => m.HealthPage)
+  },
 ];
